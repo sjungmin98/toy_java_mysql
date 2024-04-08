98a93e5578c7ed0910d31f16825cf16bd6f0a81c
sjungmin98 <shc980@gmail.com>
2024-04-08 04:03:54 +0000

    111

--- app/src/main/java/quests/AdditionsSub.java
+++ app/src/main/java/quests/AdditionsSub.java
@@ -1,12 +1,31 @@
 package quests;
 
+import java.util.Scanner;
+
 public class AdditionsSub {
-    public int additionsSubsWithParamsReturn(int firstNumber, int secondNumber) {
-        System.out.println("AdditionsSub = additionsSubsWithParamsReturn(int firstNumber, int secondNumber)");
+    private Scanner scanner;
+
+    public AdditionsSub() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int additionsSubsWithParamsReturn() {
+        System.out.print("firstNumber: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("secondNumber: ");
+        int secondNumber = scanner.nextInt();
+
+        int result = firstNumber + secondNumber;
+
+        System.out.println("AdditionsSub = additionsSubsWithParamsReturn()");
         System.out.println("params : firstNumber-" + firstNumber + ", secondNumber-" + secondNumber);
-        
-        int resultNumber = firstNumber + secondNumber;
-        
-        return resultNumber;
+        return result;
+    }
+
+    public void closeScanner() {
+        if (scanner != null) {
+            scanner.close();
+        }
     }
 }
